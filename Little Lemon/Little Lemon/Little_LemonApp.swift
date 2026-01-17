07c94843a7b11b06254f9cea6610d049bd3e883f
champanihamy <wcuhamy@outlook.com>
2026-01-17 13:38:15 +0530

    first commit

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Champani Hamy on 2026-01-17.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
